eb60acae71c759b8708c06a708f41b7fd763ba1b
amir <amirtaki623@gmail.com>
2025-05-10 15:17:25 +0330

    Access to the element  DOM

--- src/main.js
+++ src/main.js
@@ -1,1 +1,20 @@
-// document.getElementById("text").innerHTML = "Dummy Project DOM ?"
+// document.getElementById("text").innerHTML = "Access to the element DOM ?"
+// - Access to the element : - دسترسی به المنت
+
+// Get element by id  : دسترسی با آی دی
+window.document.getElementById('to-do-list')
+
+// Get element by class name : دسترسی با نام کلاس
+window.document.getElementsByClassName("to-do-item")  // => HTMLCollection
+
+let toDoItmes = document.getElementsByClassName("to-do-item") 
+
+for (let i in toDoItmes){console.log(toDoItmes[i])}
+
+
+window.document.getElementsByClassName("to-do-section") // -> HTMLCollection
+
+
+// Get element by tag name : دسترسی با نام تگ
+window.document.getElementsByTagName('li')
+
