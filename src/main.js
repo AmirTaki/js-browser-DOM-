c187fad468959eb2e19671d8755b1c7caccfc3d5
amir <amirtaki623@gmail.com>
2025-05-11 14:31:14 +0330

    The content inside the tag: DOM

--- src/main.js
+++ src/main.js
@@ -1,40 +1,13 @@
-// document.getElementById("text").innerHTML = "itration of elements: DOM ?"
+// document.getElementById("text").innerHTML = "The content inside the tag: DOM ?"
 
-// itration of elements
+// The content inside the tag
 
-document.querySelector("#to-do-list").childNodes // => return child node 
+document.getElementById("to-do-list").innerHTML  // => return  all element 
 
-document.childNodes    // => return child node html
+document.getElementById("to-do-list").textContent  // => retrunt text in element + blank space
 
-document.querySelector("#to-do-list").children // => return child type : element
+document.getElementById("to-do-list").innerText // => return text in element
 
-document.querySelector("#to-do-list").children.length  // => return number Child : type : element
+document.getElementById("ref-link").innerHTML = "<h1> Link </h1>"   // text html
 
-document.querySelector("#to-do-list").childElementCount  // => return number chileElement 
-
-document.querySelector("#to-do-list").firstChild    // => return first Child 
-
-document.querySelector("#to-do-list").firstElementChild    // => return first Child : type Element 
-
-document.querySelector("#to-do-list").lastChild             // => return last Child
-
-document.querySelector("#to-do-list").lastElementChild     // => return last Child type : element
-
-document.querySelector("#to-do-list").parentNode     // => return parent node
-
-document.querySelector("#to-do-list").parentElement // => return parent element
-
-document.querySelector("#to-do-list").parentElement.childNodes // => return node same-level Element 
-
-document.querySelector("#to-do-list").parentElement.children // => return node same-level Element  type : element
-
-document.querySelector("#main-title").nextSibling // => return node next
-
-document.querySelector("#main-title").nextElementSibling // => return node next type : element 
-
-document.querySelector("#main-title").previousSibling   // => return node back 
-
-document.querySelector("#main-title").previousElementSibling // => return node back type : element
-
-document.querySelector('.to-do-item').closest("ul")  // => return parent element specified
-document.querySelector('.to-do-item').closest(".to-do-section")
+document.getElementById("ref-link").innerText = "<h1> Link </h1>"  // text normal
